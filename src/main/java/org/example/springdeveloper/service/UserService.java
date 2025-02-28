ed2321305ec461c2491cab50113c016d08a1093c
[jisung403] <jisung494@gmail.com>
2025-02-28 14:00:37 +0900

    message

---
+++ src/main/java/org/example/springdeveloper/service/UserService.java
@@ -0,0 +1,24 @@
+package org.example.springdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.springdeveloper.domain.User;
+import org.example.springdeveloper.dto.AddUserRequest;
+import org.example.springdeveloper.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                // 패스워드 암호화
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
